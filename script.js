0cb2e734742828d9f66c07c4c19080699e1f6536
vijay saiwal <saiwal.vijay@gmail.com>
2020-12-04 12:00:07 +0530

    rest and spread operator

--- script.js
+++ script.js
@@ -1,20 +1,26 @@
-'use strict';
+"use strict";
 
 const restaurant = {
-  name: 'Classico Italiano',
-  location: 'Via Angelo Tavanti 23, Firenze, Italy',
-  categories: ['Italian', 'Pizzeria', 'Vegetarian', 'Organic'],
-  starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
-  mainMenu: ['Pizza', 'Pasta', 'Risotto'],
+  name: "Classico Italiano",
+  location: "Via Angelo Tavanti 23, Firenze, Italy",
+  categories: ["Italian", "Pizzeria", "Vegetarian", "Organic"],
+  starterMenu: ["Focaccia", "Bruschetta", "Garlic Bread", "Caprese Salad"],
+  mainMenu: ["Pizza", "Pasta", "Risotto"],
 
   order: function (starterIndex, mainIndex) {
     return [this.starterMenu[starterIndex], this.mainMenu[mainIndex]];
   },
 
-  orderDelivary: function({starterIndex, mainIndex, time, address}) {
+  orderDelivary: function ({ starterIndex, mainIndex, time, address }) {
     // console.log(
     //   `order rececived! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be deliverd to ${address} at ${time}`
     // );
+  },
+
+  orderPasta: function (ing1, ing2, ing3) {
+    console.log(
+      `Here is your delicious pasta with ${ing1}, ${ing2} and ${ing3}`
+    );
   },
 
   openingHours: {
@@ -34,51 +40,57 @@ const restaurant = {
 };
 
 restaurant.orderDelivary({
-  time: '20:30',
-  address: 'Arogya colony Shegaon',
-  mainIndex:2,
-  starterIndex: 3
+  time: "20:30",
+  address: "Arogya colony Shegaon",
+  mainIndex: 2,
+  starterIndex: 3,
 });
 
 ///////// Spread Operator ////////////
 const arrSpread = [2, 3, 4];
-const newArr = [0, 1 , ...arrSpread]
+const newArr = [0, 1, ...arrSpread];
 console.log(newArr);
 
+const ingredients = [
+  prompt("Lets's make pasta! Ingredeint 1?"),
+  prompt("Ingredeint 2?"),
+  prompt("Ingredeint 3?"),
+];
+console.log(ingredients);
+restaurant.orderPasta(...ingredients)
 
 ///////// Object Descrtucturing ////////////
 
-const { name, openingHours, categories} = restaurant;
+const { name, openingHours, categories } = restaurant;
 // console.log(name, openingHours, categories);
 
 const {
   name: restaurantName,
-  openingHours : hours,
-  categories: tags
+  openingHours: hours,
+  categories: tags,
 } = restaurant;
 
 // console.log(restaurantName, hours, tags);
 
 // DEFAULT VALUES
-const {menu = [], starterMenu: staters = []} = restaurant;
+const { menu = [], starterMenu: staters = [] } = restaurant;
 // console.log(menu, staters);
 
 // MUTATING VARIABLES
 let a = 111;
 let b = 333;
-const obj = {a:2, b:3};
-({a, b} = obj);
+const obj = { a: 2, b: 3 };
+({ a, b } = obj);
 // console.log(a,b);
 
 // NESTED OBJECT
 const {
-  fri: {open, close}
+  fri: { open, close },
 } = openingHours;
 // console.log(open, close);
 
-
 ////////// Array Descrtucturing //////////
-const arr = ['Vijay', 3, 5];
+const arr = ["Vijay", 3, 5];
 const [x, y, z] = arr;
 // console.log(`Hello ${x}`);
 // console.log(arr);
@@ -104,10 +116,9 @@ const [starter, mainCourse] = restaurant.order(2, 0);
 
 // NESTED DESCRTUCTURING
 const nested = [2, 4, [3, 5]];
-const [i, ,[j, k]] = nested;
+const [i, , [j, k]] = nested;
 // console.log(i, j, k);
 
 // DEFAULT VALUES
-const [p, q, r=1] = [5, 6]
+const [p, q, r = 1] = [5, 6];
 // console.log(p, q, r);
-
